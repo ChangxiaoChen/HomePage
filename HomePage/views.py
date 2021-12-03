e0a5d388987c6098d80b2546cceb38c27d08dcb4
Elvis.Shi <Singosgu@gmail.com>
2021-12-03 14:50:52 +0800

    V 0.0.1

---
+++ HomePage/views.py
@@ -0,0 +1,41 @@
+async def favicon(request):
+    path = str(settings.BASE_DIR) + '/static/img/logo.png'
+    content_type, encoding = mimetypes.guess_type(path)
+    resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+    resp['Cache-Control'] = "max-age=864000000000"
+    return resp
+
+
+async def css(request):
+    path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
+    content_type, encoding = mimetypes.guess_type(path)
+    resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+    resp['Cache-Control'] = "max-age=864000000000"
+    return resp
+
+
+async def js(request):
+    path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
+    content_type, encoding = mimetypes.guess_type(path)
+    resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+    resp['Cache-Control'] = "max-age=864000000000"
+    return resp
+
+from django.http import StreamingHttpResponse, JsonResponse
+from django.conf import settings
+from wsgiref.util import FileWrapper
+import mimetypes, os
+
+async def statics(request):
+    path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
+    content_type, encoding = mimetypes.guess_type(path)
+    resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+    resp['Cache-Control'] = "max-age=864000000000"
+    return resp
+
+def fonts(request):
+    path = str(settings.BASE_DIR) + '/templates/dist/spa' + request.path_info
+    content_type, encoding = mimetypes.guess_type(path)
+    resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+    resp['Cache-Control'] = "max-age=864000000000"
+    return resp
